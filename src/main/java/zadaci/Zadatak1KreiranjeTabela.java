c3f572e02b95d981befefd1c945deac05a676f2c
MilosRadin <milosradin86@gmail.com>
2020-09-28 20:16:57 +0200

    Uradjen zadatak kreiranja tabela closes #6

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -1,17 +1,36 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
 
+
 public class Zadatak1KreiranjeTabela {
+
+    static Dao<Avion,Integer> avionDao;
+    static Dao<Roba,Integer> robaDao;
+
+
     public static void main(String[] args) {
 
         ConnectionSource connectionSource = null;
         try {
 
             connectionSource = new JdbcConnectionSource(Konstante.DATABASE_URL);
+
+            TableUtils.clearTable(connectionSource,Roba.class);
+            TableUtils.clearTable(connectionSource,Avion.class);
+
+            avionDao= DaoManager.createDao(connectionSource, Avion.class);
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+
 
         }catch (Exception e) {
             e.printStackTrace();
